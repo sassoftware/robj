c6b5f76240cb13a46a6a2aaaf2b2a9f1ad754cc5
Elliot Peele <https://issues.rpath.com/>
2010-09-09 17:10:26 -0400

    add tests for the http client layer of robj

--- test/robjtest/httptest.py
+++ test/robjtest/httptest.py
@@ -19,22 +19,84 @@ from robj.http import HTTPClient
 class HTTPClientTest(testsuite.TestCase):
     def setUp(self):
         testsuite.TestCase.setUp(self)
-
-        self._client = HTTPClient(self.server.geturi('/api/'), maxClients=1)
+        self.client = HTTPClient(self.server.geturi('/api/'), maxClients=1)
 
     def testGET(self):
-        req = self._client.do_GET('/')
+        req = self.client.do_GET('/')
         req.wait()
 
-        reqs = []
-        for i in range(10):
-            reqs.append(self._client.do_GET('/'))
-
-        for req in reqs:
-            req.wait()
+        self.failUnlessEqual(self.getXML('/api'), req.response.read())
 
     def testGETError(self):
-        req = self._client.do_GET('/foobar')
+        req = self.client.do_GET('/foobar')
         req.wait()
 
-        self.failUnlessEqual(req.resp.status, 404)
+        self.failUnlessEqual(req.response.status, 404)
+
+    def testPOST(self):
+        employee1 = self.getArchiveContents('employee1.xml')
+        req = self.client.do_POST('/employees', employee1)
+        req.wait()
+
+        clientEmployee = req.response.read()
+        self.failUnlessEqual(clientEmployee, self.getXML('/api/employees/0'))
+
+    def testPOSTError(self):
+        employee1 = self.getArchiveContents('employee1.xml')
+        req = self.client.do_POST('/', employee1)
+        req.wait()
+
+        self.failUnlessEqual(req.response.status, 501)
+
+    def testPUT(self):
+        # First post some data so that we can then update it.
+        employee1 = self.getArchiveContents('employee1.xml')
+        req = self.client.do_POST('/employees', employee1)
+        req.wait()
+
+        xml = req.response.read()
+
+        # Change the employees name from Fred to Bob.
+        xml2 = xml.replace('Fred', 'Bob')
+
+        req2 = self.client.do_PUT('/employees/0', xml2)
+        req2.wait()
+
+        respxml = req2.response.read()
+
+        self.failUnlessEqual(xml2, respxml)
+        self.failUnlessEqual(respxml, self.getXML('/api/employees/0'))
+
+    def testPUTError(self):
+        req = self.client.do_GET('/')
+        req.wait()
+
+        xml = req.response.read()
+        xml2 = xml.replace('1.0', '2.0')
+
+        req2 = self.client.do_PUT('/', xml2)
+        req2.wait()
+
+        self.failUnlessEqual(req2.response.status, 501)
+
+    def testDELETE(self):
+        # First post some data so that we can then update it.
+        employee1 = self.getArchiveContents('employee1.xml')
+        req = self.client.do_POST('/employees', employee1)
+        req.wait()
+
+        req2 = self.client.do_DELETE('/employees/0')
+        req2.wait()
+
+        self.failUnlessEqual(req2.response.status, 200)
+
+        req3 = self.client.do_DELETE('/employees/0')
+        req3.wait()
+
+        self.failUnlessEqual(req3.response.status, 404)
+
+    def testDELETEError(self):
+        req = self.client.do_DELETE('/')
+        req.wait()
+
+        self.failUnlessEqual(req.response.status, 501)
